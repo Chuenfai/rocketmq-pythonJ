f1608f764502696dad67628acd64e9f3144ce5f3
chuenfai <chuenfai@chuenfai-mbp.local>
2018-08-13 16:08:50 +0800

    1608

--- settings.py
+++ settings.py
@@ -13,6 +13,8 @@ JAVA_EXT_DIRS = "-Djava.ext.dirs=" + ROCKETMQ_HOME
 
 #startJVM中的options参数不能包含空格！只能一项一项填写
 # JVM_OPTIONS = ['-server', '-Xms256m', '-Xmx256m', '-Xmn128m', JAVA_EXT_DIRS]
+JVM_OPTIONS = ('-server', '-Xms256m', '-Xmx256m', '-Xmn128m', JAVA_EXT_DIRS)
+
 JVM_HEAP_XMS = '-Xms8g'
 JVM_HEAP_XMX = '-Xmx8g'
 JVM_HEAP_XMN = '-Xmn4g'
