9e3cdaf4d874f62b5e69fcca1769b2289e13093f
chuenfai <chuenfai@chuenfai-mbp.local>
2018-08-15 15:19:53 +0800

    1519

--- pyrmq.py
+++ pyrmq.py
@@ -44,19 +44,6 @@ _SendStatusJ = JPackage('org.apache.rocketmq.client.producer').SendStatus
 _ConsumerConcurrentlyStatusJ = JPackage('org.apache.rocketmq.client.consumer.listener').ConsumerConcurrentlyStatus
 _ConsumeOrderlyStatusJ = JPackage('org.apache.rocketmq.client.consumer.listener').ConsumeOrderlyStatus
 
-
-SendStatus = {
-    'SEND_OK': _SendStatusJ.SEND_OK,
-    'FLUSH_DISK_TIMEOUT': _SendStatusJ.FLUSH_DISK_TIMEOUT,
-    'FLUSH_SLAVE_TIMEOUT': _SendStatusJ.FLUSH_SLAVE_TIMEOUT,
-    'SLAVE_NOT_AVAILABLE': _SendStatusJ.SLAVE_NOT_AVAILABLE
-}
-ConsumeConcurrentlyStatus = {
-    'SUCCESS': _ConsumerConcurrentlyStatusJ.SUCCESS,
-    'RECONSUME_LATER': _ConsumerConcurrentlyStatusJ.RECONSUME_LATER
-}
-
-
 def buildMessage(topic, tags, keys, body):
     """ body must be type of bytes.
     """
@@ -100,9 +87,9 @@ def registerListener(consumer, process):
     else:
         def consumeMessage_(messages, context):
             if process(messages):
-                return ConsumeConcurrentlyStatus['SUCCESS']
+                return _ConsumerConcurrentlyStatusJ.SUCCESS
             else:
-                return ConsumeConcurrentlyStatus['RECONSUME_LATER']
+                return _ConsumerConcurrentlyStatusJ.RECONSUME_LATER
         listener = JProxy(
             "org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently", 
             dict={'consumeMessage': consumeMessage_}
