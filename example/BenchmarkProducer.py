9c5ec9bea5f308e465a2ddeb67960ea8dd622f2e
chuenfai <chuenfai@chuenfai-mbp.local>
2018-08-16 17:19:49 +0800

    1719

--- example/BenchmarkProducer.py
+++ example/BenchmarkProducer.py
@@ -33,26 +33,26 @@ elif len(sys.argv) == 4:
     if int(sys.argv[3]):
         THREAD_NUM = sys.argv[3]
 
-m = ""
-while len(m) < MESSAGE_SIZE:
-    m += MESSAGE
-SEND_MESSAGE = buildMessage(TOPIC, '', '', m.encode('utf-8'))
+# m = ""
+# while len(m) < MESSAGE_SIZE:
+#     m += MESSAGE
+# SEND_MESSAGE = buildMessage(TOPIC, '', '', m.encode('utf-8'))
 
-COUNT_DOWN = THREAD_NUM
-def send(producer, number):
-    global SEND_MESSAGE_TOTAL, SEND_LOCK, COUNT_DOWN, SEND_MESSAGE
-    while SEND_MESSAGE_TOTAL < number:
-        sendMessage(producer, SEND_MESSAGE)
-        SEND_MESSAGE_TOTAL += 1
-    print('message send finished!')
-    # with SEND_LOCK:
-    #     COUNT_DOWN -= 1
+# COUNT_DOWN = THREAD_NUM
+# def send(producer, number):
+#     global SEND_MESSAGE_TOTAL, SEND_LOCK, COUNT_DOWN, SEND_MESSAGE
+#     while SEND_MESSAGE_TOTAL < number:
+#         sendMessage(producer, SEND_MESSAGE)
+#         SEND_MESSAGE_TOTAL += 1
+#     print('message send finished!')
+#     # with SEND_LOCK:
+#     #     COUNT_DOWN -= 1
 
-producer = buildProducer(PRODUCER_GROUP, NAMESRV)
-startProducer(producer)
-print("topic: %s, thread count: %d, message size: %d, total send: %d" % (TOPIC, THREAD_NUM, MESSAGE_SIZE, MESSAGE_NUM, ))
+# producer = buildProducer(PRODUCER_GROUP, NAMESRV)
+# startProducer(producer)
+# print("topic: %s, thread count: %d, message size: %d, total send: %d" % (TOPIC, THREAD_NUM, MESSAGE_SIZE, MESSAGE_NUM, ))
 
-tmp = int(MESSAGE_NUM / THREAD_NUM)
+# tmp = int(MESSAGE_NUM / THREAD_NUM)
 # for i in range(THREAD_NUM):
 #     if i < THREAD_NUM - 1:
 #         t = Thread(target=send, args=(producer, tmp, ))
@@ -60,7 +60,20 @@ tmp = int(MESSAGE_NUM / THREAD_NUM)
 #         t = Thread(target=send, args=(producer, MESSAGE_NUM - (i + 1) * tmp, ))
 #     t.start()
 #     print('thread %s started, which will produce %d messages.' % (t.getName(), tmp, ))
-send(producer, tmp)
+# send(producer, tmp)
+
+# def sampling(producer):
+#     global COUNT_DOWN, SEND_MESSAGE_TOTAL
+#     last = 0
+#     start = time.time()
+#     while True:
+#         time.sleep(1000)
+#         print("send TPS: %d, total message: %d" % (SEND_MESSAGE_TOTAL - last, SEND_MESSAGE_TOTAL, ))
+#         last = SEND_MESSAGE_TOTAL
+#         if COUNT_DOWN <= 0:
+#             break
+#     print("cost time: %fs" % (time.time() - start, ))
+#     shutdownProducer(producer)
 
 def sampling(producer):
     global COUNT_DOWN, SEND_MESSAGE_TOTAL
@@ -69,12 +82,12 @@ def sampling(producer):
     while True:
         time.sleep(1000)
         print("send TPS: %d, total message: %d" % (SEND_MESSAGE_TOTAL - last, SEND_MESSAGE_TOTAL, ))
+        SEND_MESSAGE_TOTAL = 0
         last = SEND_MESSAGE_TOTAL
-        if COUNT_DOWN <= 0:
+        if SEND_MESSAGE_TOTAL >= 10:
             break
     print("cost time: %fs" % (time.time() - start, ))
-    shutdownProducer(producer)
 
-# t = Thread(target=sampling, args=(producer, ))
-# t.start()
-# print('thread ' + t.getName() + 'started.\n')
+t = Thread(target=sampling, args=(producer, ))
+t.start()
+print('thread ' + t.getName() + 'started.\n')
